055450575417f8116dd1d469896523024137d0e4
Antonio Carlos <baptistaacbc@hotmail.com>
2017-09-12 23:07:49 -0300

    Adicionado whitenoise para compatibilidade com arquivos estaticos

--- meupetapi/meupetapi/production_settings/__init__.py
+++ meupetapi/meupetapi/production_settings/__init__.py
@@ -1,3 +1,5 @@
+import dj_database_url
+
 from meupetapi.settings import *
 
 DEBUG = False
@@ -9,3 +11,8 @@ ALLOWED_HOSTS = [
 ]
 
 SECRET_KEY = get_env_variable('SECRET_KEY')
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage '
