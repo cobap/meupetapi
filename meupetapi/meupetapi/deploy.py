afaa5e069ec81f27b3f1e43fa4719886abfa0dbd
Antonio Carlos <baptistaacbc@hotmail.com>
2017-09-18 23:57:25 -0300

    Adicionando suspensão de arquivos estatiscos

--- meupetapi/meupetapi/deploy.py
+++ meupetapi/meupetapi/deploy.py
@@ -16,5 +16,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meupetapi.production_settings")
 
 application = get_wsgi_application()
-
-# application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(application)
